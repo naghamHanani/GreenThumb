1f0778cb91268b7c108ca966a511c6d407d702d7
miraa2001 <miirajamous@gmail.com>
2024-06-15 21:35:37 +0300

    minor bug

--- Routes/ResourcesRouter.js
+++ Routes/ResourcesRouter.js
@@ -8,14 +8,6 @@ const {checkAuth}=require("../Middleware/checkAuth")
 const router = require('express').Router()
 
 
-
-router.get("/resources",ResourceController.getAllResources)
-router.post("/resources",ResourceController.addResource)//add checkAuthMiddleware.checkAuth,
-//the checker is a middlewear
-router.post("/deleteresource",[check("id").exists().withMessage("id is required").isNumeric().withMessage("id should be only number")],ResourceController.deleteResource)
-router.post("/editresources",ResourceController.editResource)
-router.get("/",ResourceController.getAllResources)
-
 router.get("/:id",ResourceController.getResourceById)
 
 router.post("/",checkAuth,ResourceController.addResource) //user should add token in autherizatu=ion header
